be76db220a132c62780f018f14c5be32753acba2
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-13 14:35:05 +0200

    Basic file created

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- 
+Movie class need to be defined.
+   -> inside the class are defined variables of instance
+   -> inside the class is defined a constructor
+   -> inside the class is defined at least one method
+- are instantiated at least two objects 'Movie' and printed on screen the values of the relative properties -->
+
