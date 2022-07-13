2457d9b2abf54becb913cc980b5f55deb0aa177f
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-13 16:24:34 +0200

    carousel v2.0 (still not works BOOTSTRAP -> GLIDER.JS

--- index.php
+++ index.php
@@ -29,53 +29,48 @@ $movie = [
    <title>Movies card</title>
 
    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css' integrity='sha512-o/MhoRPVLExxZjCFVBsm17Pkztkzmh7Dp8k7/3JrtNCHh0AQ489kwpfA3dPSHzKDe8YCuEhxXq3Y71eb/o6amg==' crossorigin='anonymous' />
+   <link rel="stylesheet" type="text/css" href="glider.css">
+
+   <script>
+      window.addEventListener('load', function() {
+         new Glider(document.querySelector('.glider'), {
+            setting-name: setting-value
+         })
+      })
+   </script>
 </head>
 
 <body>
    <div class="container">
       <div class="row">
          <div class="col">
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-               <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-               </div>
-               <div class="carousel-inner">
-                  <div class="carousel-item active">
-                     <img src="https://cdn.dribbble.com/users/529449/screenshots/4851203/atm_neon_dribbble.jpg" class="d-block w-100" alt="...">
-                     <div class="carousel-caption d-none d-md-block">
-                        <h5>Hello</h5>
-                        <p>Slide to see movie</p>
-                     </div>
-                  </div>
-                  <?php foreach ($movie as $value) { ?>
-                     <div class="carousel-item">
-                        <img src="<?php echo $value->image ?>" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                           <h5><?php echo $value->title ?></h5>
-                           <p><?php echo $value->description ?></p>
+            <div class="glider-container">
+               <div class="glider">
+                  <?php foreach ($movie as $movie) { ?>
+                     <div class="card" style="width: 18rem;">
+                        <img src="<?php echo $movie->image; ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                           <h5 class="card-title"><?php echo $movie->title; ?></h5>
+                           <p class="card-text"><?php echo $movie->description; ?></p>
+                           <p class="card-text"><?php echo $movie->mainActor; ?></p>
+                           <p class="card-text"><?php echo $movie->mainActor2; ?></p>
+                           <p class="card-text"><?php echo $movie->mainActor3; ?></p>
+                           <p class="card-text"><?php echo $movie->getYearsOfMovie(); ?></p>
+                           <p class="card-text"><?php echo $movie->getAgePublicSuggested(); ?></p>
                         </div>
                      </div>
                   <?php } ?>
-               </div>
-               
-               <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-               </button>
-               <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-               </button>
-            </div>
 
+                  <button aria-label="Previous" class="glider-prev">«</button>
+                  <button aria-label="Next" class="glider-next">»</button>
+                  <div role="tablist" class="dots"></div>
+               </div>
+            </div>
          </div>
       </div>
    </div>
-
-
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+   <script src="glider.js"></script>
+   <script src="app.js"></script>
 </body>
 
 </html>
